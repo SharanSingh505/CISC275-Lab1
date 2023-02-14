f89b8b678034d98b1b0efed743cb72e6e6893614
Sharanjit <sharansm@udel.edu>
2023-02-14 12:43:08 -0500

    added info 2

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -2,7 +2,7 @@
 below variables and update them with stuff about you. Yeah... I know... the classic basic info
 everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
-/*Un-comment this and fill in the info
+
 const yourName : String = "Sharanjit Singh";
 let yourPronouns : String = "He/Him";
 const yourMajor : String = "Computer Science- Data Science";
@@ -10,4 +10,3 @@ let classification : String = "Sophomore";
 let oneExcite : String = "Learning TypeScript";
 let anyConcerns : String = "I do not have any concerns";
 let funFact : String = "I like to play basketball";
-*/
